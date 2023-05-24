4c952926ce87e19fa6dd863ac75f464df9a80bce
santosrivaldo <77171407+santosrivaldo@users.noreply.github.com>
2023-05-24 16:26:37 -0300

    Master

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div className="Header">
+            <div class="toolbar">
+                <div class="toolbar_left">
+                    <span>Logo</span>
+                </div>
+                <div class="toolbar_center">
+                    <i>Buscador</i>
+                </div>
+            </div>            
+        </div>
+    )
+}
+
+
+export default Header;
---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import Header from "../../components/header/header";
+
+
+function Home() {
+    return (
+        <div className="Home" >
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
